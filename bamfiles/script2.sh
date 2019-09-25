68a8e4cfb9d5c62953de5e3bf788a4ba95751c2f
kkkaslikar <kaushal.kaslikar@gmail.com>
2019-09-26 00:49:09 +0800

    Added Gviz range plotting analysis.

---
+++ bamfiles/script2.sh
@@ -0,0 +1,2 @@
+
+macs2 bdgdiff --t1 lap-smad2/lap-smad2_treat_pileup.bdg --c1 lap-smad2/lap-smad2_control_lambda.bdg --t2 native-smad3/native-smad3_treat_pileup.bdg --c2 native-smad3/native-smad3_control_lambda.bdg --d1 31588061 --d2 19110302 --o-prefix diff_lap-smad2_vs_native-smad3
